be5193f8dc58dd9ba9bc9e54cadd8524ab3e7bf2
JPGAGHAN <jpgaghan@gmail.com>
2018-07-22 19:51:01 -0400

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,22 @@ var charDiv, oppDiv, character, opponent
 var gameContent =
 
     {
-        char0: [100, 10, 200],
-        char1: [4, 5, 40],
-        char2: [3, 15, 25],
-        char3: [5, 16, 20],
+        char0: [100, 10, 200, new Audio ("assets/sound/trex.mp3")],
+        char1: [4, 5, 40, new Audio("assets/sound/raptor.mp3")],
+        char2: [3, 15, 25, new Audio ("assets/sound/fish.mp3")],
+        char3: [5, 16, 20, new Audio("assets/sound/pterodactyl.mp3")],
+        backgroundsong: new Audio("assets/sound/jpark.mp3"),
         wins: 0,
         opponentstartHealth: 0,
         characterstartHealth: 0,
 
         attackNow: function () {
+            character[3].currentTime = 0
+            character[3].play();
             this.opponentstartHealth = opponent[2];
             this.characterstartHealth = character[2];
             character[2] = character[2] - opponent[1];
             opponent[2] = opponent[2] - character[0];
-            character[0]=character[0]*2;
             this.progressbarCalculation();
         },
 
@@ -39,16 +41,16 @@ var gameContent =
                 $(".progress").hide();
                 }   else {$("#" + charDiv).remove().clone().appendTo("#characterwin", "." + charDiv);
                         $("#prog" + charDiv[4]).css("width", "100%");
+                        character[3].pause();
+                        backgroundsound.play();
                         $("#win").show();
                         $(".progress").hide();}
             }
         },
 
-        progressbarCalculation: function(osH, csH) {
-            console.log(this.characterstartHealth)
+        progressbarCalculation: function() {
             charhealthPerc = character[2] / this.characterstartHealth * 100;
             charhealthPerc = Math.round(charhealthPerc * 100) / 100
-            console.log(charhealthPerc)
             opphealthPerc = opponent[2]/this.opponentstartHealth * 100;
             opphealthPerc = Math.round(opphealthPerc * 100) / 100
             $("#prog" + charDiv[4]).removeAttr("style");
@@ -57,16 +59,20 @@ var gameContent =
             $("#prog" + oppDiv[4]).removeAttr("style");
             $("#prog" + oppDiv[4]).css("width", opphealthPerc + "%");
             $("#prog" + oppDiv[4]).text(opphealthPerc + "%");
+            this.attackResult();
             this.winloseTest();     
         },
 
         attackResult: function() {
-            
+            $("#attackresults").html("Your current health is: " + character[2] +"hp<br>Your attack damage: " + character[0] +"hp<br>Opponents current health is: " + opponent[2] +"hp<br>Opponents attack damage:" + opponent[1] +"hp");
+            character[0]=character[0]*2;
         }
     }
 
 $(document).ready(function () {
     $(".char").click(function () {
+        gameContent.backgroundsong.loop = true;
+        gameContent.backgroundsong.play();
         if (charDiv===undefined) {
             charDiv = this.id;
             character = gameContent[this.id];
